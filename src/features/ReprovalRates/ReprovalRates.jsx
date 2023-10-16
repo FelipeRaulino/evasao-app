4cd2634193c3fc93a3b9e0b7fc935387d784a817
Felipe Raulino <felipe.raaulino29@gmail.com>
2023-10-16 14:34:45 -0300

    adicionando novos dados adicionais

--- src/features/ReprovalRates/ReprovalRates.jsx
+++ src/features/ReprovalRates/ReprovalRates.jsx
@@ -445,6 +445,142 @@ const TCCIOptions = [
       },
     },
   },
+  {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: "top",
+        labels: {
+          boxWidth: 10,
+          boxHeight: 10,
+        },
+      },
+      title: {
+        display: true,
+        text: "Taxas de reprovações da atividade de TCC I",
+      },
+      subtitle: {
+        display: true,
+        position: "top",
+        text: "1º Semestres",
+      },
+      tooltip: {
+        callbacks: {
+          label: (context) => {
+            let label = context.dataset.label || "";
+
+            if (label) {
+              label += ": ";
+            }
+
+            if (context.parsed.y !== null) {
+              label += `${context.parsed.y.toFixed(1)}%`;
+            }
+
+            return label;
+          },
+          afterBody: (context) => {
+            if (context.length === 1) {
+              const semestre = context[0].label;
+              const curso = context[0].dataset.label;
+
+              const data = taxasEvasaoTCCI.filter(
+                (item) =>
+                  item.semestreAtual === semestre && item.curso === curso,
+              );
+
+              return `\t\t\t\tQuantidade de Matrículas: ${data[0].quantidadeMatriculasSemestreAtual}\n\t\t\t\tReprovados: ${data[0].reprovados}`;
+            }
+
+            let labelFormatted = "";
+
+            context.forEach((item) => {
+              const semestreItem = item.label;
+              const cursoItem = item.dataset.label;
+
+              const dataItem = taxasEvasaoTCCI.filter(
+                (itemB) =>
+                  itemB.semestreAtual === semestreItem &&
+                  itemB.curso === cursoItem,
+              );
+
+              labelFormatted += `\n\tQuantidade de Matrículas (${dataItem[0].curso}): ${dataItem[0].quantidadeMatriculasSemestreAtual}\n\tReprovados (${dataItem[0].curso}): ${dataItem[0].reprovados}`;
+            });
+
+            return labelFormatted;
+          },
+        },
+      },
+    },
+  },
+  {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: "top",
+        labels: {
+          boxWidth: 10,
+          boxHeight: 10,
+        },
+      },
+      title: {
+        display: true,
+        text: "Taxas de reprovações da atividade de TCC I",
+      },
+      subtitle: {
+        display: true,
+        position: "top",
+        text: "2º Semestres",
+      },
+      tooltip: {
+        callbacks: {
+          label: (context) => {
+            let label = context.dataset.label || "";
+
+            if (label) {
+              label += ": ";
+            }
+
+            if (context.parsed.y !== null) {
+              label += `${context.parsed.y.toFixed(1)}%`;
+            }
+
+            return label;
+          },
+          afterBody: (context) => {
+            if (context.length === 1) {
+              const semestre = context[0].label;
+              const curso = context[0].dataset.label;
+
+              const data = taxasEvasaoTCCI.filter(
+                (item) =>
+                  item.semestreAtual === semestre && item.curso === curso,
+              );
+
+              return `\t\t\t\tQuantidade de Matrículas: ${data[0].quantidadeMatriculasSemestreAtual}\n\t\t\t\tReprovados: ${data[0].reprovados}`;
+            }
+
+            let labelFormatted = "";
+
+            context.forEach((item) => {
+              const semestreItem = item.label;
+              const cursoItem = item.dataset.label;
+
+              const dataItem = taxasEvasaoTCCI.filter(
+                (itemB) =>
+                  itemB.semestreAtual === semestreItem &&
+                  itemB.curso === cursoItem,
+              );
+
+              labelFormatted += `\n\tQuantidade de Matrículas (${dataItem[0].curso}): ${dataItem[0].quantidadeMatriculasSemestreAtual}\n\tReprovados (${dataItem[0].curso}): ${dataItem[0].reprovados}`;
+            });
+
+            return labelFormatted;
+          },
+        },
+      },
+    },
+  },
 ];
 
 const PPCTData = [
@@ -743,6 +879,208 @@ const TCCIData = [
       },
     ],
   },
+  {
+    labels: taxasEvasaoTCCI
+      .filter(
+        (item) =>
+          item.semestreAtual.includes(".1") &&
+          item.curso === "Engenharia de Software",
+      )
+      .map((item) => item.semestreAtual),
+    datasets: [
+      {
+        label: windowWidth >= 426 ? "Engenharia de Software" : "ES",
+        data: taxasEvasaoTCCI
+          .filter(
+            (item) =>
+              item.semestreAtual.includes(".1") &&
+              item.curso === "Engenharia de Software",
+          )
+          .map((item) => {
+            if (Number.isNaN(item.taxaReprovados)) return 0;
+            return item.taxaReprovados * 100;
+          }),
+        borderColor: "red",
+        backgroundColor: "red",
+      },
+      {
+        label: windowWidth >= 426 ? "Ciência da Computação" : "CC",
+        data: taxasEvasaoTCCI
+          .filter(
+            (item) =>
+              item.semestreAtual.includes(".1") &&
+              item.curso === "Ciência da Computação",
+          )
+          .map((item) => {
+            if (Number.isNaN(item.taxaReprovados)) return 0;
+            return item.taxaReprovados * 100;
+          }),
+        borderColor: "blue",
+        backgroundColor: "blue",
+      },
+      {
+        label: windowWidth >= 426 ? "Sistemas de Informação" : "SI",
+        data: taxasEvasaoTCCI
+          .filter(
+            (item) =>
+              item.semestreAtual.includes(".1") &&
+              item.curso === "Sistemas de Informação",
+          )
+          .map((item) => {
+            if (Number.isNaN(item.taxaReprovados)) return 0;
+            return item.taxaReprovados * 100;
+          }),
+        borderColor: "green",
+        backgroundColor: "green",
+      },
+      {
+        label: windowWidth >= 426 ? "Engenharia de Computação" : "EC",
+        data: taxasEvasaoTCCI
+          .filter(
+            (item) =>
+              item.semestreAtual.includes(".1") &&
+              item.curso === "Engenharia de Computação",
+          )
+          .map((item) => {
+            if (Number.isNaN(item.taxaReprovados)) return 0;
+            return item.taxaReprovados * 100;
+          }),
+        borderColor: "orange",
+        backgroundColor: "orange",
+      },
+      {
+        label: windowWidth >= 426 ? "Redes de Computadores" : "RC",
+        data: taxasEvasaoTCCI
+          .filter(
+            (item) =>
+              item.semestreAtual.includes(".1") &&
+              item.curso === "Redes de Computadores",
+          )
+          .map((item) => {
+            if (Number.isNaN(item.taxaReprovados)) return 0;
+            return item.taxaReprovados * 100;
+          }),
+        borderColor: "darkgray",
+        backgroundColor: "darkgray",
+      },
+      {
+        label: windowWidth >= 426 ? "Design Digital" : "DD",
+        data: taxasEvasaoTCCI
+          .filter(
+            (item) =>
+              item.semestreAtual.includes(".1") &&
+              item.curso === "Design Digital",
+          )
+          .map((item) => {
+            if (Number.isNaN(item.taxaReprovados)) return 0;
+            return item.taxaReprovados * 100;
+          }),
+        borderColor: "darkblue",
+        backgroundColor: "darkblue",
+      },
+    ],
+  },
+  {
+    labels: taxasEvasaoTCCI
+      .filter(
+        (item) =>
+          item.semestreAtual.includes(".2") &&
+          item.curso === "Engenharia de Software",
+      )
+      .map((item) => item.semestreAtual),
+    datasets: [
+      {
+        label: windowWidth >= 426 ? "Engenharia de Software" : "ES",
+        data: taxasEvasaoTCCI
+          .filter(
+            (item) =>
+              item.semestreAtual.includes(".2") &&
+              item.curso === "Engenharia de Software",
+          )
+          .map((item) => {
+            if (Number.isNaN(item.taxaReprovados)) return 0;
+            return item.taxaReprovados * 100;
+          }),
+        borderColor: "red",
+        backgroundColor: "red",
+      },
+      {
+        label: windowWidth >= 426 ? "Ciência da Computação" : "CC",
+        data: taxasEvasaoTCCI
+          .filter(
+            (item) =>
+              item.semestreAtual.includes(".2") &&
+              item.curso === "Ciência da Computação",
+          )
+          .map((item) => {
+            if (Number.isNaN(item.taxaReprovados)) return 0;
+            return item.taxaReprovados * 100;
+          }),
+        borderColor: "blue",
+        backgroundColor: "blue",
+      },
+      {
+        label: windowWidth >= 426 ? "Sistemas de Informação" : "SI",
+        data: taxasEvasaoTCCI
+          .filter(
+            (item) =>
+              item.semestreAtual.includes(".2") &&
+              item.curso === "Sistemas de Informação",
+          )
+          .map((item) => {
+            if (Number.isNaN(item.taxaReprovados)) return 0;
+            return item.taxaReprovados * 100;
+          }),
+        borderColor: "green",
+        backgroundColor: "green",
+      },
+      {
+        label: windowWidth >= 426 ? "Engenharia de Computação" : "EC",
+        data: taxasEvasaoTCCI
+          .filter(
+            (item) =>
+              item.semestreAtual.includes(".2") &&
+              item.curso === "Engenharia de Computação",
+          )
+          .map((item) => {
+            if (Number.isNaN(item.taxaReprovados)) return 0;
+            return item.taxaReprovados * 100;
+          }),
+        borderColor: "orange",
+        backgroundColor: "orange",
+      },
+      {
+        label: windowWidth >= 426 ? "Redes de Computadores" : "RC",
+        data: taxasEvasaoTCCI
+          .filter(
+            (item) =>
+              item.semestreAtual.includes(".2") &&
+              item.curso === "Redes de Computadores",
+          )
+          .map((item) => {
+            if (Number.isNaN(item.taxaReprovados)) return 0;
+            return item.taxaReprovados * 100;
+          }),
+        borderColor: "darkgray",
+        backgroundColor: "darkgray",
+      },
+      {
+        label: windowWidth >= 426 ? "Design Digital" : "DD",
+        data: taxasEvasaoTCCI
+          .filter(
+            (item) =>
+              item.semestreAtual.includes(".2") &&
+              item.curso === "Design Digital",
+          )
+          .map((item) => {
+            if (Number.isNaN(item.taxaReprovados)) return 0;
+            return item.taxaReprovados * 100;
+          }),
+        borderColor: "darkblue",
+        backgroundColor: "darkblue",
+      },
+    ],
+  },
 ];
 
 const handleOnFormatAdditionalDataPPCT = (unformattedDataPPCT) => {
@@ -821,6 +1159,31 @@ const handleOnFormatAdditionalDataPPCT = (unformattedDataPPCT) => {
   return additionalData;
 };
 
+const handleOnFormatAdditionalDataTCC = (unformattedDataTCC) =>
+  taxasEvasaoTCCI
+    .filter((item) => item.curso === "Ciência da Computação")
+    .map((item) => item.semestreAtual)
+    .map((item) => {
+      return {
+        semestre: item,
+        dados: unformattedDataTCC
+          .filter((itemB) => itemB.semestreAtual === item)
+          .map((itemC) => {
+            return {
+              curso: itemC.curso,
+              quantidadeCurso: itemC.quantidadeMatriculasSemestreAtual,
+              semestre: itemC.semestreAtual,
+              aprovados: itemC.aprovados,
+              reprovados: itemC.reprovados,
+              reprovadosFalta: 0,
+              cancelados: 0,
+              trancados: itemC.trancados,
+              suprimidos: itemC.suprimidos,
+            };
+          }),
+      };
+    });
+
 const ReprovalRates = () => (
   <div className="content-container">
     <h1>Taxas de Reprovações</h1>
@@ -857,6 +1220,28 @@ const ReprovalRates = () => (
       options={TCCIOptions[0]}
       data={TCCIData[0]}
       title="Relação: Cursos por semestre - TCC I (Geral)"
+      additionalData={handleOnFormatAdditionalDataTCC(taxasEvasaoTCCI)}
+      additionalDataTitle="TCC I - Geral"
+    />
+
+    <Chart
+      options={TCCIOptions[1]}
+      data={TCCIData[1]}
+      title="Relação: Cursos por semestre - TCC I (1º Semestres)"
+      additionalData={handleOnFormatAdditionalDataTCC(taxasEvasaoTCCI).filter(
+        (item) => item.semestre.includes("1"),
+      )}
+      additionalDataTitle="TCC I - 1º Semestres"
+    />
+
+    <Chart
+      options={TCCIOptions[2]}
+      data={TCCIData[2]}
+      title="Relação: Cursos por semestre - TCC I (2º Semestres)"
+      additionalData={handleOnFormatAdditionalDataTCC(taxasEvasaoTCCI).filter(
+        (item) => item.semestre.includes("2"),
+      )}
+      additionalDataTitle="TCC I - 2º Semestres"
     />
   </div>
 );
